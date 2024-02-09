cf1451834b7fd2d09bed0d6439c820abb2f5ae3c
0xafrogeek <babouceesay@users.noreply.github.com>
2024-02-09 13:57:59 +0000

    initial commit

---
+++ csv_to_json.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import json
+
+with open("attributeDataValues.json") as f:
+    data = json.load(f)["trackedEntityInstances"]
+
+# Flatten to dic
+for obj in data:
+    if isinstance(obj["attributes"], list):
+        attributes_dict = {
+            attr.get("code", attr["attribute"]): attr["value"]
+            for attr in obj["attributes"]
+        }
+        obj.update(attributes_dict)
+        del obj["attributes"]
+    else:
+        print(f"Unexpected attributes type in object: {obj}")
+
+df = pd.DataFrame(data)
+
+df.to_csv("attributeDataValues.csv", index=False)
